e06f53666936561561ccb2a0196af1b30852241c
Marsender <contact@atoll-digital-library.org>
2016-03-26 14:49:41 +0100

    Remove old files

--- old/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$db_user = "mobugs";
-$db_pass = "sH#4J-s3";
-$db_host = "mobugs.db.3241389.hostedresource.com";
-$db_database = "mobugs";
-
-?>
--- old/logout.php
+++
@@ -1,6 +0,0 @@
-<?
-	session_start();
-	unset($_SESSION['valid_user']);
-	unset($current_member);
-	header("Location: ./index.php");
-?>
