e2caf6bc0ec851e71b007105a038b6218643e514
Marsender <contact@atoll-digital-library.org>
2016-03-26 14:48:34 +0100

    Remove runtime files

--- protected/runtime/gii-1.1.8/ModelCode.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array (
-  'template' => 'default',
-  'tablePrefix' => 'bk_',
-  'modelPath' => 'application.models',
-  'baseClass' => 'CActiveRecord',
-  'buildRelations' => '1',
-);
