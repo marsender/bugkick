8cf05fd11bbc77f35dd4985f9c48b835a1878a7d
Marsender <contact@atoll-digital-library.org>
2016-03-26 10:22:44 +0100

    Add date config file

---
+++ devconf/params/date.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'formatDate12'=>'d-m-Y g:ia', // format date and time to 12h format
+    'formatDateShort'=>'d M', // format date to format:  oct 12
+    'formatDateLong'=>'dS F Y', // format date to format:  03th December 2011
+    'formatDateSlash'=>'j/n/y', // format date to format:  2/25/12 - mon/day/year
+    'formatDateSlashFull'=>'j/n/y \a\t g:ia', // format date to format:  2/25/12 at 10:38pm - mon/day/year at time
+    'formatDateLongWithTime'=>'jS F Y - g:ia', // format date to format:  2nd April 2012 - 3:42pm
+);
