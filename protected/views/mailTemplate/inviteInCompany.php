829e30163d7d3b5f79075795e9f16f939f28b245
Marsender <contact@atoll-digital-library.org>
2016-04-04 04:56:57 +0100

    Manage invite

--- protected/views/mailTemplate/inviteInCompany.php
+++ protected/views/mailTemplate/inviteInCompany.php
@@ -5,7 +5,7 @@
 		<p>&nbsp;</p>
 		<p>
 			<?php echo sprintf(Yii::t('main', '%s has invited you to join a project on %s'), User::current()->getUserName(), Yii::app()->name); ?>.<br />
-			<?php echo sprintf(Yii::t('main', 'Please click the following link to %s your account'), CHtml::link(Yii::t('main', 'Confirm'), $acceptUrl)); ?>.
+			<?php echo Yii::t('main', 'Please login to your account'); ?>.
 		<p>&nbsp;</p>
 		<p><?php echo Yii::t('main', 'Yours'); ?>,<br /><?php echo User::current()->getUserName(); ?></p>
 
@@ -14,9 +14,9 @@
 			<tr>
 				<td><a
 					style="display: block; text-decoration: none; color: white; background-color: #1A74B0; margin: 10px; padding: 10px;"
-					href="<?php echo $acceptUrl; ?>"><?php echo Yii::t('main', 'Confirm'); ?></a></td>
+					href="<?php echo $acceptUrl; ?>"><?php echo Yii::t('main', 'Login'); ?></a></td>
 				<td style="margin: 10px; color: #666; font-size: 11px;">
-<?php echo Yii::t('main', 'To Confirm your account, visit this link'); ?>:<br>
+<?php echo Yii::t('main', 'To login to your account, visit this link'); ?>:<br>
 <?php echo CHtml::link($acceptUrl, $acceptUrl); ?>
 </td>
 			</tr>
